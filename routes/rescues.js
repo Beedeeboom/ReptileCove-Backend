b93c55a0e92e556f4ef808581b288983a4514d77
HeyitsmeTazG <HeyitsmeTazG@outlook.com>
2020-07-21 15:14:02 +1000

    Setting up rescues data

--- routes/rescues.js
+++ routes/rescues.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var router = express.Router()
 
+const catchers = [{name: "A rescue", location: "place"}, {name: "enother rescue", location: "another place"}]
+
 /* GET rescues */
 router.get('/', function(req, res, next) {
   res.send("<h1>Rescues</h1>")
