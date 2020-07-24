ab7c759c0a4b51e487aa7788f2f4375909184f56
Beedeeboom <stephanie.duffieux@gmail.com>
2020-07-24 12:41:29 +1000

    trying to fix login logout issues

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ app.use(function(req, res, next) {
 	res.status(err.status || 500)
 	res.render('error')
   })
-  
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
